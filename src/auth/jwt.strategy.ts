3a0ec502768226df81cef29ceb16f7a394f69151
DimitriRomano <dimitri.romano09@gmail.com>
2024-02-02 09:11:49 +0100

    fix suppression password retour profile*

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,6 +2,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UserEntity } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
@@ -14,7 +15,9 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   }
 
   async validate(payload: { userId: string }) {
-    const user = await this.usersService.findOne(payload.userId);
+    const user = new UserEntity(
+      await this.usersService.findOne(payload.userId),
+    );
 
     if (!user) {
       throw new UnauthorizedException();
